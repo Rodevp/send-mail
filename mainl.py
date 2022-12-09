f5c2c39587609ca0e7361228da4ce1452a5e8843
Rodevp <rodriguezrodrigor12@gmail.com>
2022-12-09 17:17:18 -0500

    email config

--- mainl.py
+++ mainl.py
@@ -4,11 +4,19 @@ from email.message import EmailMessage
 
 
 def main() :
+
     subject = ""
     body = ""
     email_endpoint = ""
-    receive_emial = ""
+    receive_email = ""
     password = input("Enter password: ")
+
+    email_message = EmailMessage()
+    email_message["From"] = email_endpoint
+    email_message["To"] = receive_email
+    email_message["Subject"] = subject
+    email_message.set_content(body)
+
     
 
 

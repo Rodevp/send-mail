a4fe75b9fc4c12ae655cacccad17f87c3b6d7c8e
Rodevp <rodriguezrodrigor12@gmail.com>
2022-12-09 17:13:59 -0500

    get data

---
+++ mainl.py
@@ -0,0 +1,16 @@
+import smtplib as smtp
+import ssl
+from email.message import EmailMessage
+
+
+def main() :
+    subject = ""
+    body = ""
+    email_endpoint = ""
+    receive_emial = ""
+    password = input("Enter password: ")
+    
+
+
+if __name__ == "__main__" :
+    main()
